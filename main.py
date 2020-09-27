0e4a041fbeacf33b8a2b4d95f03c9a7abbfc90d2
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2020-09-27 19:39:28 +0200

    my z3 solver trials

--- main.py
+++ main.py
@@ -64,19 +64,59 @@ def tr():
         print(m)
 
 
+def move_check(state: list, col_a: int, col_b: int) -> bool:
+    if len(state[col_a]) == 0:
+        return False
+    if len(state[col_b]) > 0:
+        return min(state[col_a]) < min(state[col_b])
+    else:
+        return True
+
+
+def move_op(state: list, col_a: int, col_b: int) -> None:
+    if move_check(state, col_a, col_b):
+        el = min(state[col_a])
+        state[col_a].remove(el)
+        state[col_b].append(el)
+
+
+move_names = ["MOVE 1 -> 2", "MOVE 2 -> 1",
+              "MOVE 1 -> 3", "MOVE 3 -> 1",
+              "MOVE 2 -> 3", "MOVE 3 -> 2"]
+
+
+def move(state: list, op: int):
+    return If(op == 0, move_op(state, 0, 1),
+              If(op == 1, move_op(state, 1, 0),
+                 If(op == 2, move_op(state, 0, 2),
+                    If(op == 3, move_op(state, 2, 0),
+                       If(op == 4, move_op(state, 1, 2),
+                          move_op(state, 2, 1))))))
+
+
 def hanoi():
-    ROWS = 4
-    COLS = 3
-    STEPS = ROWS * COLS
     s = Solver()
-    X = [[Int("x_r%d_c%d" % (i + 1, j + 1)) for j in range(COLS)] for i in range(ROWS)]
-    op = [Int('op_%d' % n) for n in range(STEPS)]
+    stack = [[1, 2, 3, 4], [], []]
+    goal = [[], [1, 2, 3, 4], []]
+    # state spaces
+    L = BitVec('L', 4)  # left
+    C = BitVec('C', 4)  # center
+    R = BitVec('R', 4)  # right
+    # init state
+    for i in range(4):
+        if i in stack[0]:
+            L |= 2**i
+        if i in stack[1]:
+            C |= 2**i
+        if i in stack[2]:
+            R |= 2**i
     #
-    #
-    #
-    #
-    print(X)
-    print(op)
+    # solution
+    #r = s.check()
+    #print(r)
+    #if r == sat:
+    #    m = s.model()
+    #    print(m)
 
 
 if __name__ == '__main__':
