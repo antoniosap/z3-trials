bc8d5ab4f1456e4242dd08639f11a205fa2a9b27
Antonio Sapuppo <antoniosapuppo@yahoo.it>
2020-10-27 08:04:45 +0100

    my z3 solver trials

--- g15-puzzle.py
+++ g15-puzzle.py
@@ -6,6 +6,7 @@
 from z3 import *
 
 BOARDS = 2
+MOVE_DOWN, MOVE_UP, MOVE_LEFT, MOVE_RIGHT = 0, 1, 2, 3
 
 X = [[[Int("x_r%s_c%s_t%s" % (i + 1, j + 1, k + 1))
        for j in range(4)]
@@ -28,9 +29,11 @@ def g15():
     distinct_c = [Distinct([X[k][i][j] for i in range(4) for j in range(4) for k in range(BOARDS)])]
     final_state_c = [X[0][i][j] == final_state[i][j] for i in range(4) for j in range(4)]
     init_state_c = [X[BOARDS - 1][i][j] == init_state[i][j] for i in range(4) for j in range(4)]
+    op = [Int('op_%d' % n) for n in range(BOARDS - 1)]
+    moves_c = [X[k - 1][i][j] == move_tile(op[k], X, i, j) for i in range(4) for j in range(4) for k in range(BOARDS)]
 
     s = Solver()
-    s.add(cells_c + distinct_c + init_state_c + final_state_c)
+    s.add(cells_c + distinct_c + init_state_c + final_state_c + moves_c)
     r = s.check()
     print(r)
     if r == sat:
@@ -40,6 +43,10 @@ def g15():
                for i in range(4)]
               for k in range(BOARDS)]
         print(ev)
+
+
+def move_null():
+    return []
 
 
 def move_down(board, row, col):
@@ -52,7 +59,7 @@ def move_down(board, row, col):
         return []
 
 
-def move_up(row, col):
+def move_up(board, row, col):
     def move(board, row, col):
         return [X[board - 1][row - 1][col] == X[board][row][col], X[board - 1][row][col] == 0]
 
@@ -62,7 +69,7 @@ def move_up(row, col):
         return []
 
 
-def move_left(row, col):
+def move_left(board, row, col):
     def move(board, row, col):
         return [X[board - 1][row][col - 1] == X[board][row][col], X[board - 1][row][col] == 0]
 
@@ -72,7 +79,7 @@ def move_left(row, col):
         return []
 
 
-def move_right(row, col):
+def move_right(board, row, col):
     def move(board, row, col):
         return [X[board - 1][row][col + 1] == X[board][row][col], X[board - 1][row][col] == 0]
 
@@ -82,29 +89,12 @@ def move_right(row, col):
         return []
 
 
-def move_check(state, col_a, col_b):
-    if len(state[col_a]) == 0:
-        return False
-    if len(state[col_b]) > 0:
-        return min(state[col_a]) < min(state[col_b])
-    else:
-        return True
-
-
-def move_op(state, col_a, col_b):
-    if move_check(state, col_a, col_b):
-        el = min(state[col_a])
-        state[col_a].remove(el)
-        state[col_b].append(el)
-
-
-def move(state, op):
-    return If(op == 0, move_op(state, 0, 1),
-              If(op == 1, move_op(state, 1, 0),
-                 If(op == 2, move_op(state, 0, 2),
-                    If(op == 3, move_op(state, 2, 0),
-                       If(op == 4, move_op(state, 1, 2),
-                          move_op(state, 2, 1))))))
+def move_tile(op, board, row, col):
+    return If(op == MOVE_DOWN, move_down(board, row, col),
+              If(op == MOVE_UP, move_up(board, row, col),
+                 If(op == MOVE_LEFT, move_left(board, row, col),
+                    If(op == MOVE_RIGHT, move_right(board, row, col),
+                          move_null()))))
 
 
 if __name__ == '__main__':
